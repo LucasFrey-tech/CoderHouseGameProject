f20df2711cc9993de04523ae466b60ca4fca8595
LucasFrey-tech <lucasfrey19@gmail.com>
2022-12-01 23:01:39 -0300

    Bridge

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{   
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        appear();
+    }
+
+    void appear(){
+        if(!GameObject.Find("FirstEnemySpawn") && gameObject.transform.position.y < 55f){
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 10, gameObject.transform.position.z), speed * Time.deltaTime);
+        }
+        return;
+    }
+
+}
