5e2f714d1bfaec5e9301ac58a9ff7f2dead8d0b4
LucasFrey-tech <lucasfrey19@gmail.com>
2022-12-04 16:38:55 -0300

    checkpoints control script

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -24,25 +24,29 @@ public class CheckPoint : MonoBehaviour
 
     void activate(){
         if(check && first && !checkPoint2.GetComponent<MeshRenderer>().enabled && !checkPoint2.GetComponent<BoxCollider>().enabled && !checkPoint2.GetComponent<PlayerSpawn>().enabled){
-            GameObject.Find("SpawnPlayer").SetActive(false);
             first = false;
         }else if(!first){
-            if(!GameObject.Find("SecondSpawn") && !GameObject.Find("ThirdSpawn") && !GameObject.FindWithTag("Enemy")){
+            if(!GameObject.Find("SecondSpawn") && !GameObject.Find("ThirdSpawn") && !GameObject.FindWithTag("Enemy") || (GameObject.FindWithTag("Enemy") && (GameObject.FindWithTag("Enemy").GetComponent<Enemy>().mySpawn.name != "SecondSpawn" && GameObject.FindWithTag("Enemy").GetComponent<Enemy>().mySpawn.name != "ThirdSpawn"))){
                 checkPoint1.GetComponent<MeshRenderer>().enabled = true;
                 checkPoint1.GetComponent<BoxCollider>().enabled = true;
+                if(GameObject.Find("SpawnPlayer")){
+                    GameObject.Find("SpawnPlayer").SetActive(false);
+                }
                 if(!GameObject.FindWithTag("Player")){
                     checkPoint1.GetComponent<PlayerSpawn>().enabled = true;
                 }
             }
         }
 
-        if(GameObject.Find("FourthSpawn") && GameObject.Find("FifthSpawn") && GameObject.Find("SixthSpawn") && (GameObject.Find("FourthSpawn").GetComponent<EnemySpawn>().cantEnemies <= 0 && GameObject.Find("FifthSpawn").GetComponent<EnemySpawn>().cantEnemies <= 0 && GameObject.Find("SixthSpawn").GetComponent<EnemySpawn>().cantEnemies <= 0) && !GameObject.FindWithTag("Enemy")){
+        if(GameObject.Find("SpawnControl").GetComponent<ActivateSpawn>().deActivated){
             checkPoint1.SetActive(false);
             checkPoint2.SetActive(true);
             checkPoint2.GetComponent<MeshRenderer>().enabled = true;
+            checkPoint2.GetComponent<BoxCollider>().enabled = true;
             checkPoint2.GetComponent<PlayerSpawn>().enabled = true;
         }else{
             checkPoint2.GetComponent<MeshRenderer>().enabled = false;
+            checkPoint2.GetComponent<BoxCollider>().enabled = false;
             checkPoint2.GetComponent<PlayerSpawn>().enabled = false;
         }
 
