ae6816e8eb46b6fc1d5db0f7d49b9401a0834f4e
LucasFrey-tech <lucasfrey19@gmail.com>
2022-12-04 16:40:30 -0300

    win and lose functions and lifes UI

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,8 +1,14 @@
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class Game : MonoBehaviour
 {   
-    public GameObject pauseUI;
+    public GameObject pauseUI, winUI, loseUI;
+    public int lifeCounter = 3;
+    public TextMeshProUGUI lifes;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,13 +18,19 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        lifes.text = lifeCounter.ToString();
         pause();
+        lose();
+        win();
     }
 
     void pause(){
-        if(Input.GetKeyDown(KeyCode.Escape)){
+        if(Input.GetKeyDown(KeyCode.Escape) && !GameObject.FindWithTag("Player").GetComponent<Animator>().GetBool("Win")){
             Time.timeScale = 0;
             pauseUI.SetActive(true);
+        }else if(GameObject.FindWithTag("Player") && GameObject.FindWithTag("Player").GetComponent<Animator>().GetBool("Win")){
+            Time.timeScale = 1;
+            pauseUI.SetActive(false);
         }
         return;
     }
@@ -29,7 +41,32 @@ public class Game : MonoBehaviour
 
     public void quit(){
         Time.timeScale = 1;
+        if(winUI){
+            winUI.SetActive(false);
+        }
+        if(loseUI){
+            loseUI.SetActive(false);
+        }
         SceneManager.LoadScene(0);
     }
 
+    void win(){
+        if(GameObject.FindWithTag("Win").transform.childCount > 0 && GameObject.FindWithTag("Win").transform.GetChild(0).tag == "Player"){
+            winUI.SetActive(true);
+        }
+        return;
+    }
+
+    void lose(){
+        if(lifeCounter <=0){
+            loseUI.SetActive(true);
+            if(GameObject.FindWithTag("Player")){
+                GameObject.FindWithTag("Player").GetComponent<Character>().enabled = false;
+                if(GameObject.Find("CM FreeLook1")){
+                    GameObject.Find("CM FreeLook1").SetActive(false);
+                }
+            }
+        }
+        return;
+    }
 }
